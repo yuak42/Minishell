5f52d4ce371b1924a05ed24ef85114789a8aa6f1
yuak42 <yuak@student.42istanbul.com.tr>
2026-02-28 17:18:48 +0300

    feat: Add basic readline behaviour
    There is still reachable but it is not a problem

--- project/include/prompt.h
+++ project/include/prompt.h
@@ -1,6 +1,12 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 void	basic_prompt(void);
 
 #endif
--- project/prompt/basic_prompt.c
+++ project/prompt/basic_prompt.c
@@ -1,6 +1,14 @@
+#include "prompt.h"
+
 void	basic_prompt(void)
 {
-
-
-
+	char	*line;
+	
+	line = readline("msh$ ");
+	if (line)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	clear_history();
 }
