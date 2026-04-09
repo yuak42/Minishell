a7f2c74cf8913106f1832bcdf991db4e0a36842c
Baran Yaprak <baran.yaprak.by>
2026-04-09 00:11:49 +0000

    feat: enhance cd command with home directory handling and error messages

--- built-in/cd.c
+++ built-in/cd.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+        */
+/*   By: byaprak <byaprak@student.42istanbul.com.tr>  #+#  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026-03-31 15:10:04 by byaprak           #+#    #+#             */
 /*   Updated: 2026-03-31 15:10:04 by byaprak          ###   ########.fr       */
@@ -12,43 +12,91 @@
 
 #include "builtin.h"
 
-static t_list	*is_home(t_list *env)
+// static t_list	*is_home(t_list *env)
+// {
+// 	while (env)
+// 	{
+// 		if (ft_strnstr(env->content, "HOME=", 5))
+// 			return (env);
+// 		env = env->next;
+// 	}
+// 	return (NULL);
+// }
+
+static int	get_home(t_list *env)
 {
-	while (env)
+	t_list	*node;
+	int		ex;
+
+	node = ft_getenv(env, "HOME");
+	if (node)
 	{
-		if (ft_strnstr(env->content, "HOME=", 5))
-			return (env);
-		env = env->next;
+		ex = chdir(node->content + 5);
 	}
-	return (NULL);
+	else
+	{
+		ex = -2;
+		ft_perror("-minishell: cd: %s not set\n", "HOME");
+	}
+	return (ex);
+}
+
+static void pwd_update(t_list *env)
+{
+	char	*content;
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (!env || !pwd)
+		return ;
+	content = ft_strjoin("PWD=", pwd);
+	if (!content)
+	{
+		free(pwd);
+		return ;
+	}
+	free(env->content);
+	env->content = content;
+	free(pwd);
+}
+
+static void old_pwd_update(t_list *env, char *here)
+{
+	char	*content;
+
+	if (!env)
+		return ;
+	content = ft_strjoin("OLDPWD=", here);
+	if (!content)
+		return ;
+	free(env->content);
+	env->content = content;
 }
 
 int	ft_cd(char **av, t_list *env)
 {
 	int		ex;
-	t_list	*node;
-	char	*path;
+	char	*here;
 
-	node = NULL;
 	av++;
+	here = getcwd(NULL, 0);
+	if (!here)
+		return (1);
 	if (!*av)
-	{
-		node = is_home(env);
-		if (node)
-		{
-			path = node->content +5;
-			ex = chdir(path);
-		}
-		else
-			ex = -1;
-	}
+		ex = get_home(env);
 	else
 		ex = chdir(*av);
-	if (ex == -1)
+	if (ex == -1 || ex == -2)
 	{
-		perror("");
+		if (ex == -1)
+			ft_perror("minishell: cd: %s: No such file or directory\n", *av);
+		free(here);
 		return (1);
 	}
-	return(0);
+	pwd_update(ft_getenv(env, "PWD"));
+	old_pwd_update(ft_getenv(env, "OLDPWD"), here);
+	free(here);
+	return (0);
 }
+//env'de pwd ve old pwd update et.
 
