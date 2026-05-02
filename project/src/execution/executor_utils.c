52421ef34bc01980b303596ea1cecf5571f1cd98
Baran Yaprak <baran.yaprak.by>
2026-05-02 20:58:19 +0000

    fix: refine is_builtin function to ensure correct command length checks

--- project/src/execution/executor_utils.c
+++ project/src/execution/executor_utils.c
@@ -12,19 +12,19 @@
 
 #include"prompt.h"
 
-int is_builtin(t_node *nodes, t_shell *shell)
+int	is_builtin(t_node *nodes, t_shell *shell)
 {
-	if (ft_strnstr(nodes->argv[0], "cd", ft_strlen(nodes->argv[0])))
+	if (ft_strnstr(nodes->argv[0], "cd", 2) && ft_strlen(nodes->argv[0]) == 2)
 		return (ft_cd(nodes->argv, shell->ev));
-	else if (ft_strnstr(nodes->argv[0], "echo", ft_strlen(nodes->argv[0])))
+	else if (ft_strnstr(nodes->argv[0], "echo", 4) && ft_strlen(nodes->argv[0]) == 4)
 		return (ft_echo(nodes->argv, shell->ev));
-	else if (ft_strnstr(nodes->argv[0], "env", ft_strlen(nodes->argv[0])))
+	else if (ft_strnstr(nodes->argv[0], "env", 3) && ft_strlen(nodes->argv[0]) == 3)
 		return (ft_env(shell->ev));
-	else if (ft_strnstr(nodes->argv[0], "export", ft_strlen(nodes->argv[0])))
+	else if (ft_strnstr(nodes->argv[0], "export", 6) && ft_strlen(nodes->argv[0]) == 6)
 		return (ft_export(nodes->argv, shell->ev));
-	else if (ft_strnstr(nodes->argv[0], "pwd", ft_strlen(nodes->argv[0])))
+	else if (ft_strnstr(nodes->argv[0], "pwd", 3) && ft_strlen(nodes->argv[0]) == 3)
 		return (ft_pwd());
-	else if (ft_strnstr(nodes->argv[0], "unset", ft_strlen(nodes->argv[0])))
+	else if (ft_strnstr(nodes->argv[0], "unset", 5) && ft_strlen(nodes->argv[0]) == 5)
 		return (ft_unset(nodes->argv, &shell->ev));
 	else
 		return (0);
