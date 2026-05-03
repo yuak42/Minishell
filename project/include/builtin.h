6474e354a44719da44864a18b6049250280d0f27
Baran Yaprak <baran.yaprak.by>
2026-05-03 15:59:50 +0000

    refactor: update is_builtin and ft_run_process function signatures for improved parameter clarity

--- project/include/builtin.h
+++ project/include/builtin.h
@@ -14,7 +14,7 @@ int		ft_cd(char **av, t_env *env);
 int		ft_unset(char **av, t_env **env);
 void	execute(t_node *nodes, t_shell *shell);
 char	**env_to_arry(t_shell *shell);
-int		is_builtin(t_node *nodes, t_shell *shell);
+int		is_builtin(char **argv, t_shell *shell);
 int		ft_env_size(t_env *lst);
 
 
