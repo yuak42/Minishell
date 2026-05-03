7cf9c5c89b4b5c6a55b810778aaee2696a6b38cc
Baran Yaprak <baran.yaprak.by>
2026-05-03 15:58:59 +0000

    refactor: update ft_child and ft_pipex function signatures for improved clarity and consistency

--- project/src/pip_src/pipex.c
+++ project/src/pip_src/pipex.c
@@ -12,7 +12,7 @@
 
 #include "pipex.h"
 
-void	ft_child(char *argv, char **envp, int std_int, int std_out)
+void	ft_child(char *argv, t_env *envp, int std_int, int std_out)
 {
 	char	**cmd;
 	char	*path;
@@ -34,6 +34,7 @@ void	ft_child(char *argv, char **envp, int std_int, int std_out)
 	}
 	close(std_int);
 	close(std_out);
+	
 	ft_run_process(path, cmd, envp);
 }
 
@@ -63,29 +64,27 @@ int	ft_process(t_pipe plist, int (*pipefd)[2], int pc, int *pd)
 	return (pid);
 }
 
-int	ft_pipex(char **av, char **ev, int ac)
+int	ft_pipex(t_node *node, t_env *ev, int ac)
 {
 	int	(*fd)[2];
 	int	*pd;
 	int	i;
 	int	status;
 
-	pd = malloc(sizeof(int) * (ac - 3));
-	fd = malloc(sizeof(*fd) * (ac - 4));
+	pd = malloc(sizeof(int) * (ac));
+	fd = malloc(sizeof(*fd) * (ac - 1));
 	ft_pip(ac, fd);
-	pd[0] = ft_process(ft_struct(av[2], ev, ft_of(av[1]), fd[0][1]),
-			fd, ac - 4, pd);
+	//pd[0] = ft_process(ft_struct(node, ev, fd, 0), fd, ac - 1, pd);
 	i = 1;
-	while (i < ac - 4)
+	while (i < ac)
 	{
-		pd[i] = ft_process(ft_struct(av[i + 2], ev, fd[i - 1][0], fd[i][1]),
-				fd, ac - 4, pd);
+		pd[i] = ft_process(ft_struct(node, ev, fd, i), fd, ac - 1, pd);
 		i++;
 	}
-	pd[ac - 4] = ft_process(ft_struct(av[ac - 2], ev, fd[ac - 5][0],
-				ft_cf(av[ac - 1])), fd, ac - 4, pd);
-	ft_pipeclose(fd, ac - 4);
-	status = ft_wait(pd, ac - 3);
+	// if (ac != 1)
+	// 	pd[ac - 1] = ft_process(ft_struct(node, ev, fd, ac - 2), fd, ac - 1, pd);
+	ft_pipeclose(fd, ac - 1);
+	status = ft_wait(pd, ac - 1);
 	free(fd);
 	free(pd);
 	exit (WEXITSTATUS(status));
