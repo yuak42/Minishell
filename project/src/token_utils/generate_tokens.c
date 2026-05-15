d7e5e613555804ab16420e0e6ec30fbe5632ea5e
yuak42 <yuak@student.42istanbul.com.tr>
2026-05-15 11:14:16 +0300

    fix: split end of the line fixed

--- project/src/token_utils/generate_tokens.c
+++ project/src/token_utils/generate_tokens.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 11:48:35 by yuak              #+#    #+#             */
-/*   Updated: 2026/05/11 17:13:42 by yuak             ###   ########.fr       */
+/*   Updated: 2026/05/15 11:13:27 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ t_token	*generate_tokens(char *line)
 	{
 		while (line[i] == ' ' || line[i] == '\t')
 			i++;
+		if (!line[i])
+			break ;
 		if (is_operator(line[i]))
 			token = get_operator(line, &i);
 		else
