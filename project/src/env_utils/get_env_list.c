4fb6eac449c2215747f25624d4dfef2115360464
yuak42 <yuak@student.42istanbul.com.tr>
2026-05-16 19:01:15 +0300

    fix: add error check to env_list

--- project/src/env_utils/get_env_list.c
+++ project/src/env_utils/get_env_list.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 11:54:19 by yuak              #+#    #+#             */
-/*   Updated: 2026/05/15 12:54:24 by yuak             ###   ########.fr       */
+/*   Updated: 2026/05/16 19:00:29 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,15 @@ t_env	*get_env_list(char **env)
 	head = NULL;
 	while (*env)
 	{
-		ev_node = (t_env *) malloc(sizeof(t_env)); // add error check later
+		ev_node = (t_env *) malloc(sizeof(t_env));
+		if (!ev_node)
+			return (free_ev(head), NULL);
 		ev_node->key = get_key(*env);
+		if (!ev_node->key)
+			return (free(ev_node), free_ev(head), NULL);
 		ev_node->value = get_value(*env);
+		if (!ev_node->value)
+			return (free(ev_node->key), free(ev_node), free_ev(head), NULL);
 		ev_node->next = NULL;
 		if (head == NULL)
 		{
@@ -47,41 +53,50 @@ t_env	*get_env_list(char **env)
 
 static char	*get_key(char *env)
 {
-	// '=' e kadar olan kısmı kopyalayacak
 	char	*str;
 	char	*key;
 	size_t	i;
 
 	str = env;
 	i = 0;
-	while (str[i] != '=')
+	while (str[i] != '=' && str[i] != '\0')
 		i++;
-	key = (char *) malloc(sizeof(char) * (i + 1)); // add error check later
+	key = (char *) malloc(sizeof(char) * (i + 1));
+	if (!key)
+		return (NULL);
 	i = 0;
-	while (str[i] != '=') //buralara belki string sonu kosulu eklenebilir sonra
+	while (str[i] != '=' && str[i] != '\0')
 	{
 		key[i] = str[i];
 		i++;
 	}
 	key[i] = '\0';
-	return key;
+	return (key);
 }
 
 static char	*get_value(char *env)
 {
-	// '=' den sona kadar olan kısmı kopyalayacak
 	char	*str;
 	char	*value;
 	size_t	i;
 
 	str = env;
 	i = 0;
-	while (*str != '=')
+	while (*str != '=' && *str)
 		str++;
+	if (*str == '\0')
+	{
+		value = ft_strdup("");
+		if (!value)
+			return (NULL);
+		return (value);
+	}
 	str++;
 	while (str[i])
 		i++;
-	value = (char *) malloc(sizeof(char) * (i + 1)); // add error check later
+	value = (char *) malloc(sizeof(char) * (i + 1));
+	if (!value)
+		return (NULL);
 	i = 0;
 	while (str[i])
 	{
@@ -89,5 +104,5 @@ static char	*get_value(char *env)
 		i++;
 	}
 	value[i] = '\0';
-	return value;
+	return (value);
 }
