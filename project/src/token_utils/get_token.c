f8a7923222cdd7a55de8c4d17a69c971693e2600
yuak42 <yuak@student.42istanbul.com.tr>
2026-05-17 19:24:43 +0300

    fix: wrong throw syntax error

--- project/src/token_utils/get_token.c
+++ project/src/token_utils/get_token.c
@@ -6,67 +6,57 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 11:53:05 by yuak              #+#    #+#             */
-/*   Updated: 2026/05/17 15:50:51 by yuak             ###   ########.fr       */
+/*   Updated: 2026/05/17 19:22:41 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prompt.h"
 
 static int	is_metachar(char c);
+size_t	get_end_of_word(char *line);
 
 t_token	*get_word(char *line, size_t *i)
 {
 	t_token	*token;
-	size_t	start;
 	size_t	j;
-	int		quote;
 
-	j = (*i);
-	if (line[j] == '\'')
-		quote = 1;
-	else if (line[j] == '"')
-		quote = 2;
-	else
-		quote = 0;
 	token = (t_token *) ft_calloc(1, sizeof(t_token));
 	if (!token)
 		return (print_error("Error: ft_calloc\n"), NULL);
-	start = j;
-	j++;
-	if (quote == 1)
-	{
-		while (quote == 1 || (!is_metachar(line[j]) && line[j] != '\0'))
-		{
-			if (line[j] == '\'')
-				quote = 0;
-			if (line[j] == '\0')
-				break ;
-			j++;
-		}
-	}
-	else if (quote == 2)
-	{
-		while (quote == 2 || (!is_metachar(line[j]) && line[j] != '\0'))
-		{
-			if (line[j] == '"')
-				quote = 0;
-			if (line[j] == '\0')
-				break ;
-			j++;
-		}
-	}
-	else
-	{
-		while (!is_metachar(line[j]) && line[j] != '\0')
-			j++;
-	}
-	*i = j;
-	token->value = ft_substr(line, start, j - start);
+	j = get_end_of_word(&line[*i]);
+	token->value = ft_substr(line, *i, j);
 	if (!token->value)
 		return (free(token), NULL);
 	token->type = token_word;
 	token->next = NULL;
+	*i = *i + j;
 	return (token);
+}
+
+size_t	get_end_of_word(char *line)
+{
+	size_t	j;
+
+	j = 0;
+	while (line[j])
+	{
+		if (line[j] == '"')
+		{
+			j++;
+			while (line[j] && line[j] != '"')
+				j++;
+		}
+		if (line[j] == '\'')
+		{
+			j++;
+			while (line[j] && line[j] != '\'')
+				j++;
+		}
+		if (is_metachar(line[j]) || line[j] == '\0')
+			return (j);		
+		j++;
+	}
+	return (j);
 }
 
 t_token	*get_operator(char *line, size_t *i)
