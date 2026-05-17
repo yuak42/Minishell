8477741e0df51b84aebe33a14fb557f3da999d02
yuak42 <yuak@student.42istanbul.com.tr>
2026-05-17 23:20:22 +0300

    fix

--- project/src/token_utils/tokenizer.c
+++ project/src/token_utils/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 12:49:03 by yuak              #+#    #+#             */
-/*   Updated: 2026/05/17 19:47:42 by yuak             ###   ########.fr       */
+/*   Updated: 2026/05/17 23:18:02 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_token	*tokenizer(char *line, t_shell *shell)
 		shell->exit_status = 1;
 		return (NULL);
 	}
-	print_tokens(shell->tokens);
+	// print_tokens(shell->tokens);
 	if (!is_syntax_correct(shell->tokens))
 	{
 		print_error("syntax error!\n");
