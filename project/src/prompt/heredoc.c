cc1f44ff216d9032dbe034fff1806bf096443567
yuak42 <yuak@student.42istanbul.com.tr>
2026-05-19 12:19:08 +0300

    feat: heredoc

--- project/src/prompt/heredoc.c
+++ project/src/prompt/heredoc.c
@@ -6,20 +6,19 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 09:53:55 by yuak              #+#    #+#             */
-/*   Updated: 2026/05/19 11:20:54 by yuak             ###   ########.fr       */
+/*   Updated: 2026/05/19 11:48:15 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prompt.h"
 
+static int	get_input(t_redir *redir);
+
 int	heredoc(t_shell *shell)
 {
 	t_node	*head;
 	t_redir	*redir;
-	char	*str;
-	int		p[2];
 	
-	pipe(p);
 	head = shell->nodes;
 	while (head)
 	{
@@ -28,14 +27,47 @@ int	heredoc(t_shell *shell)
 		{
 			if (redir->type == token_heredoc)
 			{
-				str = readline("> ");
-				write(p[1], str, ft_strlen(str));
-				redir->read = p[0];
-				free(str);
+				if (get_input(redir))
+					return (1);
+				
 			}
 			redir = redir->next;
 		}
 		head = head->next;
 	}
+	return (0);
+}
+
+static int	get_input(t_redir *redir)
+{
+	int		p[2];
+	char	*line;
+
+	if (pipe(p) == -1)
+		return (1);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+		{
+			ft_perror("minishell: warning: here-document delimited by end-of-file (wanted `%s`)\n", redir->file);
+			close(p[1]);
+			redir->read = p[0];
+			return (1);
+		}
+		if (ft_strlen(line) == ft_strlen(redir->file)
+			&& ft_strncmp(line, redir->file, ft_strlen(line)) == 0)
+		{
+			free(line);
+			break ;
+		}
+		// expansion later using heredoc_exp is true or not
+		write(p[1], line, ft_strlen(line));
+		write(p[1], "\n", 1);
+		free(line);
+		
+	}
+	close(p[1]);
+	redir->read = p[0];
 	return (0);
 }
