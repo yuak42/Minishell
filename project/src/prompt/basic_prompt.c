013d7b6bfee8a16592734761be98c0fad6ffa690
yuak42 <yuak@student.42istanbul.com.tr>
2026-05-19 09:50:59 +0300

    fix: norm

--- project/src/prompt/basic_prompt.c
+++ project/src/prompt/basic_prompt.c
@@ -6,13 +6,15 @@
 /*   By: yuak <yuak@student.42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 11:53:22 by yuak              #+#    #+#             */
-/*   Updated: 2026/05/19 09:39:29 by yuak             ###   ########.fr       */
+/*   Updated: 2026/05/19 09:50:44 by yuak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prompt.h"
 
 static char	*prompt(void);
+static t_shell	*parser(t_shell *shell);
+static void	free_parser(t_shell *shell);
 
 void	basic_prompt(t_shell *shell)
 {
@@ -29,21 +31,33 @@ void	basic_prompt(t_shell *shell)
 			continue ;
 		}
 		shell->line = line;
-		shell->tokens = tokenizer(line, shell);
-		if (shell->tokens)
-		{
-			shell->nodes = create_nodes(shell->tokens);
-			if (shell->nodes)
-			{
-				print_nodes(shell->nodes);
-				execute(shell->nodes, shell);
-				free_nodes(shell->nodes);
-			}
-		}
-		free_tokens(shell->tokens);
-		free(line);
+		shell = parser(shell);
+		if (!shell)
+			continue ;
+		// heredoc()
+		execute(shell->nodes, shell);
+		free_parser(shell);
 	}
 	rl_clear_history();
+}
+
+static t_shell	*parser(t_shell *shell)
+{
+	shell->tokens = tokenizer(shell->line, shell);
+	if (shell->tokens)
+	{
+		shell->nodes = create_nodes(shell->tokens);
+		if (shell->nodes)
+			print_nodes(shell->nodes);
+		else
+		{
+			free_tokens(shell->tokens);
+			return (free(shell->line), NULL);
+		}
+	}
+	else
+		return (free(shell->line), NULL);
+	return (shell);
 }
 
 static char	*prompt(void)
@@ -63,4 +77,11 @@ static char	*prompt(void)
 	}
 	add_history(line); // belki daha sonra (heredoc'tan) sonraya koyulabilir.
 	return (line);
+}
+
+static void	free_parser(t_shell *shell)
+{
+	free_nodes(shell->nodes);
+	free_tokens(shell->tokens);
+	free(shell->line);
 }
