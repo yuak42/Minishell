abd5e66d03d06b8bbce99afbe72b131ec69c8cd3
Baran Yaprak <baran.yaprak.by>
2026-05-20 15:02:18 +0000

    fix: Norm Fix 25 lines

--- project/src/execution/execute.c
+++ project/src/execution/execute.c
@@ -12,35 +12,43 @@
 
 #include "prompt.h"
 
+static int	void_args(t_node *nodes)
+{
+	if (!ft_redir(nodes))
+	{
+		perror("-minishell");
+		return (1);
+	}
+	else
+		return (0);
+}
+
+static int	run_prompt(t_node *nodes, t_shell *shell, int fd)
+{
+	int	status;
+
+	ft_redir(nodes);
+	if (nodes->infile == -1)
+	{
+		perror("-minishell");
+		return (1);
+	}
+	else if (nodes->outfile)
+		fd = nodes->outfile;
+	status = run_builtin(nodes->argv, shell, fd);
+	return (status);
+}
+
 void	execute(t_node *nodes, t_shell *shell)
 {
-	int status;
+	int	status;
 	int	fd;
 
 	fd = 1;
-	if (!nodes->next && ((!nodes->argv) || !nodes->argv[0])) // || nodes->argv[0][0] == '\0'))
-	{
-		if(!ft_redir(nodes))
-		{
-			perror("-minishell");
-			status = 1;
-		}
-		else
-			status = 0;
-	}
+	if (!nodes->next && ((!nodes->argv) || !nodes->argv[0]))
+		status = void_args(nodes);
 	else if (!nodes->next && is_builtin(nodes->argv))
-	{
-		ft_redir(nodes);
-		if (nodes->infile == -1)
-		{
-			perror("-minishell");
-			shell->exit_status = 1;
-			return ;
-		}
-		if (nodes->outfile)
-			fd = nodes->outfile;
-		status = run_builtin(nodes->argv, shell, fd);
-	}
+		status = run_prompt(nodes, shell, fd);
 	else
 		status = ft_pipex(nodes, shell, nodes_len(nodes));
 	shell->exit_status = status;
