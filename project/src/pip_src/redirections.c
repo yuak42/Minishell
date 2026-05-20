30fb40bdcf7f5353e285e193a9f887a756b51508
Baran Yaprak <baran.yaprak.by>
2026-05-20 16:11:38 +0000

    fix: Norm Fix 25 lines

--- project/src/pip_src/redirections.c
+++ project/src/pip_src/redirections.c
@@ -12,6 +12,47 @@
 
 #include "prompt.h"
 
+static int	in_redir(t_redir *redir, int infile)
+{
+	int	i_file;
+
+	if (redir->type == token_redir_in)
+	{
+		if (infile > 2)
+			close(infile);
+		i_file = ft_of(redir->file);
+	}
+	else if (redir->type == token_heredoc)
+	{
+		if (redir->read)
+		{
+			if (infile > 2)
+				close (infile);
+			i_file = redir->read;
+		}
+	}
+	return (i_file);
+}
+
+static int out_redir(t_redir *redir, int outfile)
+{
+	int	o_file;
+
+	if (redir->type == token_redir_out)
+	{
+		if (outfile > 2)
+			close(outfile);
+		o_file = ft_cf(redir->file);
+	}
+	else if (redir->type == token_redir_app)
+	{
+		if (outfile)
+			close(outfile);
+		o_file = ft_af(redir->file);
+	}
+	return (o_file);
+}
+
 int	ft_redir(t_node *nodes)
 {
 	t_redir	*head;
@@ -23,33 +64,10 @@ int	ft_redir(t_node *nodes)
 	head = nodes->redir;
 	while (head)
 	{
-		if (head->type == token_redir_out)
-		{
-			if (outfile > 2)
-				close(outfile);
-			outfile = ft_cf(head->file);
-		}
-		else if (head->type == token_redir_in)
-		{
-			if (infile > 2)
-				close(infile);
-			infile = ft_of(head->file);
-		}
-		else if (head->type == token_redir_app)
-		{
-			if (outfile)
-				close(outfile);
-			outfile = ft_af(head->file);
-		}
-		else if (head->type == token_heredoc)
-		{
-			if (head->read)
-			{
-				if (infile > 2)
-					close (infile);
-				infile = head->read;
-			}
-		}
+		if (head->type == token_redir_out || head->type == token_redir_app)
+			outfile = out_redir(nodes->redir, outfile);
+		else if (head->type == token_redir_in || head->type == token_heredoc)
+			infile = in_redir(nodes->redir, infile);
 		if (infile == -1 || outfile == -1)
 			break ;
 		head = head->next;
